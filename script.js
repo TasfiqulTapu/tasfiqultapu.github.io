16ef34f10533b1b6a6b3dc7e2e68c569bb0aa103
Tapu <66732331+TasfiqulTapu@users.noreply.github.com>
2021-12-07 16:42:06 +0600

    Fixed timezone

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const clock = document.querySelector('#time');
 setInterval(() => {
     //get gmt+6 time
-    let date = new Date().toLocaleString('en-GB', {timeZone: 'Asia/Bangkok',day: 'numeric', month: 'long', year: 'numeric'});
-    let tiem = new Date().toLocaleString('en-GB', {timeZone: 'Asia/Bangkok',hour: 'numeric', minute: 'numeric', second: 'numeric'});
+    let date = new Date().toLocaleString('en-GB', {timeZone: 'Asia/Dhaka',day: 'numeric', month: 'long', year: 'numeric'});
+    let tiem = new Date().toLocaleString('en-GB', {timeZone: 'Asia/Dhaka',hour: 'numeric', minute: 'numeric', second: 'numeric'});
     clock.innerHTML = `${date} • ${tiem}`;
 }, 1000);
 
